53c39f1931299542d5362e527dcbb8cb9738bb6d
Nick <nicktaylor1110@gmail.com>
2020-11-15 17:57:00 +0000

    Added more HDAs. Sorted icons. Added example hip creator script for future use. Removed presets dir (Technical_new.desk only).

---
+++ examples/create_examples.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import sys, os, shutil
+
+def create_hips():
+    ROOT = Path(os.path.dirname(os.path.realpath(__file__))).parent
+
+    hda_dir = ROOT.joinpath("otls")
+    hdas = list(hda_dir.glob("*.hda"))
+    template = ROOT.joinpath("examples/BASE.hip")
+
+    for h in hdas:
+        dst = ROOT.joinpath("examples")
+        dst = dst.joinpath(h.stem + ".hip")
+        shutil.copy(template, dst)
+
+create_hips()
